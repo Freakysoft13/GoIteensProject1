8b9680cd6ab77470d030ef1dd36ff151de97b419
Loiso <bogdanrybak71@gmail.com>
2018-03-18 21:36:28 +0200

    minor changes

--- Assets/Scripts/Lesson3_Ex1.cs
+++ Assets/Scripts/Lesson3_Ex1.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class Lesson3_Ex1 : MonoBehaviour
 {
 
-	public int a;
-	public int b;
+	 int a = 10;
+	 int b = 43;
 
-	void MyFunction ()
+	void Start ()
 	{
-		if (a == b) {
+		if (a != b) 
+		{
+			if (a + b <= 52) {
+				print ("Lemon");
+			} else if (a / 2 - b == -38 && a * b < 100) {
+				print ("Apple");
+			} else if (b >= a && b <= 0) {
+				print ("Pear");
+			} else if (a * b < 25 || a == 10) {
+				print ("Kivi");
+			} else
+				print ("Potato");
+		
+		} 
+		else
 			print ("Numbers are equal");
-		} else
-			print ("Numbers are not equal");
 	}
 
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,41 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	public float speed;
+	public float speed = 10.0f;
+	public float gravity = -9.8f;
+	public float jumpheight = 3.0f;
 
-	private Rigidbody rb;
+	private CharacterController _charCont;
 
 	void Start ()
 	{
-		rb = GetComponent<Rigidbody>();
+		_charCont = GetComponent<CharacterController> ();
 	}
-	/*
-	void FixedUpdate ()
-	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
-		rb.AddForce (movement * speed);
-	}
-    */
-	void Update()
+	void Update ()
 	{
-		
-		if (Input.GetKey (KeyCode.W)) {
-			transform.Translate (Vector3.left * speed);
-		}
-		if (Input.GetKey (KeyCode.S)) {
-			transform.Translate (Vector3.right * speed);
-		}
-		if (Input.GetKey (KeyCode.A)) {
-			transform.Translate (Vector3.back * speed);
-		}
-		if (Input.GetKey (KeyCode.D)) {
-			transform.Translate (Vector3.forward * speed);
+		float deltaX = Input.GetAxis ("Horizontal") * speed;
+		float deltaZ = Input.GetAxis ("Vertical") * speed;
+		Vector3 movement = new Vector3 (deltaX, 0, deltaZ);
+		movement = Vector3.ClampMagnitude (movement, speed);
+
+		movement.y = gravity;
+
+		movement *= Time.deltaTime;
+		movement = transform.TransformDirection (movement);
+		_charCont.Move (movement);
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Vector3 jump = new Vector3 (0, jumpheight, 0);
+			jump = Vector3.ClampMagnitude(jump, jumpheight);
+			_charCont.Move (jump);
 		}
 	}
 }
